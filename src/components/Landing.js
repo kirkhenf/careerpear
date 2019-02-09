7693d65bb672f7ff973b5ef058044bfd0d538868
Kirk Henf <kirkhenf@Kirks-MacBook-Pro.local>
2019-02-08 19:50:47 -0800

    restyling landing page with new text

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,6 +6,7 @@ const LandingPage = () =>
   <div className="banner">
     <div className="banner__content">
       <h1 className="header">You're here. That means you're one step closer to a better career.</h1>
+      <h3>Take our pear-ing assessment to see how we can help</h3>
       <Button size="large" variant="contained">Get Started</Button>
     </div>
   </div>
