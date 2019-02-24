6d5f62fa00505e2de54bf250f75183fcd7c16bef
Kirk Henf <kirkhenf@Kirks-MacBook-Pro.local>
2019-02-23 20:13:50 -0800

    getting results sent for user

--- src/actions/Quiz.js
+++ src/actions/Quiz.js
@@ -3,14 +3,22 @@
  */
 
 import { getFirestore } from 'redux-firestore';
+import { getFirebase } from 'react-redux-firebase';
 
 export const addQuizResults = (results) => async dispatch => {
     var fStorm = getFirestore();
-    console.log(fStorm);
-    fStorm.collection('users').doc().set(results);
+    var fBase = getFirebase();
+    fBase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            fStorm.collection('users').doc(user.uid).set(results);
+        } else {
+            console.log("No user logged in.")
+        }
+    });
+    
     // return "test"
 }
 
 // export const fetchToDos = () => async dispatch => {
-    
+
 // };
