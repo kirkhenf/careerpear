a17be21256d763d9ff6811f697b7b7b2fd6653fe
Mayoor Rai <mayoorrai@Mayoors-MacBook-Pro-2.local>
2019-02-24 19:51:18 -0500

    more comments

--- src/components/Authentication/Account.js
+++ src/components/Authentication/Account.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
 
+// File imports
 import { PasswordForgetForm } from './PasswordForget';
 import PasswordChangeForm from './PasswordChange';
 import withAuthorization from './withAuthorization';
